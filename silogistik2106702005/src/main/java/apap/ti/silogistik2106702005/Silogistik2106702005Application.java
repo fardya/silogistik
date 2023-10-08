33fa4b65ab7cbbda98f6627906a7be483b1038aa
fardya <davina.fardya@ui.ac.id>
2023-10-08 14:55:20 +0700

    chore: add database dependencies

---
+++ silogistik2106702005/src/main/java/apap/ti/silogistik2106702005/Silogistik2106702005Application.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106702005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Silogistik2106702005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Silogistik2106702005Application.class, args);
+	}
+
+}
